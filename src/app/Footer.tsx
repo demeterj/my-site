49a3ff422a5bb34ea81f9f1002287b7378a455e3
Jonathan Demeter <demeterj@umich.edu>
2024-08-07 15:09:11 -0400

    Reduce footer padding

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Footer() {
   const year = new Date().getFullYear()
   return (
-    <div className="flex justify-around py-8 bg-blue-100 border-t-2 border-solid border-black">
+    <div className="flex justify-around py-6 bg-blue-100 border-t-2 border-solid border-black">
       <div className="flex flex-col">
         <span className="font-bold">Email</span>
         <a href="mailto:demeterj@umich.edu" className="underline">demeterj@umich.edu</a>
