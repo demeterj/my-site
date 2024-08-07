00affe0655f373d83d99621bf2624e0045323ac4
Jonathan Demeter <demeterj@umich.edu>
2024-08-07 14:59:26 -0400

    Update Logo to my initials

--- src/app/Logo.tsx
+++ src/app/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
 
   return (
     <div className="flex justify-between">
-      <a href="/"><span className="text-2xl font-bold"> Jonathan Demeter </span></a>
+      <a href="/"><span className="text-2xl font-bold border-solid border-2 border-black p-1 rounded-md"> JD </span></a>
     </div>
   ) 
 }
