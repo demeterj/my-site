275275eb6fe16ba383e7e4c76a6bfbe9554371a1
Jonathan Demeter <demeterj@umich.edu>
2024-08-14 15:16:02 -0400

    Import arrow SVG on resume page as SVGR react component

--- src/app/work/Header.tsx
+++ src/app/work/Header.tsx
@@ -1,12 +1,16 @@
-import Image from "next/image"
+import Arrow from "@/assets/Arrow.svg"
 
 export default function ResumeHeader() {
   return (
     <div className="flex justify-between items-center mb-[10px]">
       <div className="font-bold text-[40px]">Resume</div>
-      <a href="https://drive.google.com/file/d/1e0bQpX5gyUvdkCuM-3dWFBGeN-gOwZAk/view?usp=sharing" target="_blank">
+      <a
+        href="https://drive.google.com/file/d/1e0bQpX5gyUvdkCuM-3dWFBGeN-gOwZAk/view?usp=sharing"
+        target="_blank"
+        className="p-2 group transition-all ease-in-out delay-150 duration-500 hover:text-slate-600"
+        >
         View Full Resume
-        <Image src="./arrow.svg" alt="arrow" width={20} height={20} className="inline ml-1" />
+          <Arrow width={18} height={18} className="group-hover:text-slate-600 inline ml-1 group-hover:translate-x-1 group-hover:-translate-y-1 transition-all ease-in-out delay-150 duration-200"/>
         </a>
     </div>
   )
