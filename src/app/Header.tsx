dabf580b5153ff9d94d7c5b17026462c4ffbf137
Jonathan Demeter <demeterj@umich.edu>
2024-08-21 16:12:42 -0400

    Replace header and footer with expandable menu

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,55 +1,26 @@
-import Navigator from "../components/Navigator";
+'use client'
+import { useState } from "react";
 import Logo from "./Logo";
+import Menu from "./Menu";
 
 export default function Header() {
-  const initialRoutes = [
-    {
-      name: 'Home',
-      path: '/',
-      parameters: [],
-      anchor: ''
-    },
-    {
-      name: 'Work',
-      path: '/work',
-      parameters: [],
-      anchor: ''
-    },
-    {
-      name: 'About Me',
-      path:'/about-me',
-      anchor: ''
-    },
-    {
-      name: 'Contact',
-      path:'/contact-me',
-      anchor: ''
-    }
-  ];
+  let [menuOpen, setMenuOpen] = useState(false);
+  function handleMenuClick() {
+    setMenuOpen(!menuOpen)
+  }
   return (
-    <div 
-      className={`
-        sticky
-        top-[-60px]
-        hover:top-0
-        z-10
-        w-full
-        flex
-        items-center
-        justify-between
-        p-4
-      bg-blue-100
-        border-b-2
-        border-solid
-        border-black
-        transition-all
-        ease-in-out
-        delay-150
-      `}
-    >
-      <Logo />
-      <Navigator routes={initialRoutes} />
-      <div className="absolute transparent h-[50px] bottom-[-50px] w-full"></div>
-    </div>
+    <>
+      <div className="fixed top-8 left-8 z-10">
+        <Logo />
+      </div>
+      <button className="fixed top-[43px] left-[100px] z-10" onClick={handleMenuClick}>MENU</button>
+      <a
+        href="mailto:demeterj@umich.edu"
+        className="fixed top-[43px] right-[40px] z-10"
+      >
+        CONTACT ME
+      </a>
+      {menuOpen && <Menu closeMenu={handleMenuClick} />}
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,25 @@ const config: Config = {
         "primary-text-color": "rgb(var(--primary-text-color))",
         "secondary-text-color": "rgb(var(--secondary-text-color))"
       }
+    },
+    animation: {
+      fadeIn: 'fadeIn 1s ease-in-out var(--slidein-delay, 0) forwards',
+      dropIn: 'dropIn 300ms ease-in forwards',
+      scale: 'scale 1s ease-in-out forwards'
+    },
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: '0', transform: 'translate(0px, -5px)'},
+        '100%': { opacity: '1', transform: 'translate(0, 0)' }
+      },
+      dropIn: {
+        '0%': { transform: 'translateY(-100%)', opacity: '0' },
+        '100%': { transform: 'translateY(0)', opacity: '1' }
+      },
+      scale: {
+        '0%': { transform: 'scale(1)' },
+        '100%': { transform: 'scale(1.2)' }
+      }
     }
   },
   plugins: [],
