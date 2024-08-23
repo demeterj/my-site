6240d7f5eeed970bc4ba5622865d599b3a3b34d3
Jonathan Demeter <demeterj@umich.edu>
2024-08-23 12:41:07 -0400

    Minor layout change on home page
    Switched the profile picture and intro blurb around for better space management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ const myBio = `I'm a software engineer and product manager who's motivated by bu
 
 export default function Home() {
   return (
-    <div className="h-full flex justify-around items-center w-full border-solid border-red-500">
-      <div className="w-[700px]">
-        <p className="text-4xl font-bold">Hi, I'm <span className="text-blue-900">Jonathan Demeter!</span></p>
-        <p className="text-2xl">{myBio}</p>
-      </div>
+    <div className="flex justify-around items-center w-full h-full pl-[40px]">
       <ContentBox>
         <Image
           src="/ProfilePicture.JPG"
@@ -21,6 +17,10 @@ export default function Home() {
           Returning to the States after backpacking in Japan for three weeks!
         </div>
       </ContentBox>
+      <div className="w-[600px]">
+        <p className="text-4xl font-bold">Hi, I'm <span className="text-blue-900">Jonathan Demeter!</span></p>
+        <p className="text-2xl">{myBio}</p>
+      </div>
     </div>
   );
 }
